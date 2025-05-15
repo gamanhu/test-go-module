abc8690deb6201f19468a816138a798654dd41e0
Khoi-Nguyen Nguyen-Hoang <khoi-nguyen.nguyen-hoang@ekino.com>
2025-05-15 09:22:15 +0700

    change greetings message

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,6 @@ package greetings
 import "fmt"
 
 func Hello(name string) string {
-	message := fmt.Sprintf("Hi and Hello %v. Welcome!", name)
+	message := fmt.Sprintf("Hi and Hello baba %v. Welcome!", name)
 	return message
 }
